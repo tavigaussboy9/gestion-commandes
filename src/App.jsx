30fc649bc79a068c333626a66e3d663275da8f02
tavigaussboy9 <prieuxolowo@hotmail.com>
2025-10-09 23:44:41 +0400

    feat: initial commit for src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './components/Menu';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Gestion des Commandes</h1>
+      <Menu />
+    </div>
+  );
+};
+
+export default App;
